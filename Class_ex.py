fd8e48787000b4a2ba02ceb200c36fe0eccf6546
Rohit Verma <48760327+rohitverma229@users.noreply.github.com>
2019-08-06 14:30:33 +0530

    initial commit

---
+++ Class_ex.py
@@ -0,0 +1,14 @@
+class Cal(object):
+    # pi is a class variable
+    pi = 3.142
+
+    def __init__(self, radius):
+        # self.radius is an instance variable
+        self.radius = radius
+
+    def area(self):
+        return self.pi * (self.radius ** 2)
+    
+obj = Cal(2)
+obj.pi
+obj.area()
